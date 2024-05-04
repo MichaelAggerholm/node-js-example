01b6c781542e96a875fa41de1e9e1638e94d149c
Michael Aggerholm <92aggerholm@gmail.com>
2024-05-04 08:09:13 +0200

    test number two

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World - From Nodejs web example.');
+    res.send('Hello World - From Nodejs web example number two.');
 });
 
 const PORT = process.env.PORT || 3000;
